51b44280d714fee9b16c1e01eb46a889608b9f4e
2nt0 <53119527+2nt0@users.noreply.github.com>
2022-12-23 20:02:15 +0000

    Update v2.py
    major changes, should output all data in a nice-ish format for TCP and UDP

--- v2.py
+++ v2.py
@@ -1,6 +1,10 @@
 import socket
 import struct
 
+#define <take the last two chars> function
+def tltc(string):
+        return string[-2:]
+
 # Create a raw socket
 raw_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
 
@@ -18,13 +22,36 @@ while True:
     # Unpack the packet header
     ethernet_header = packet[0][0:14]
     ethernet_header = struct.unpack("!6s6s2s", ethernet_header)
-
-    # Extract the source and destination MAC addresses
-    source_mac = ethernet_header[0]
-    dest_mac = ethernet_header[1]
-
-    # Print the packet information
+    
+    #extract, format and print source and header macs
+    source_mac = ":".join(map(tltc, map(hex, map(int, ethernet_header[0])))).upper()
+    dest_mac = ":".join(map(tltc, map(hex, map(int, ethernet_header[1])))).upper() 
     print("\nSource MAC: {}\tDestination MAC: {}".format(source_mac, dest_mac))
-    for i in packet:
-        print(i)
-        #open("log.log", "wb").write(i)
+    
+    if sum(list(map(int, ethernet_header[2]))) == 2048:
+        #ipv4 packet
+        ip_header = struct.unpack('!BBHHHBBH4s4s', packet[0][14:34])
+        ip_protocol = ip_header[6]
+        print("IP header data:  ", ip_header)
+        
+        #extract, format and print src and dst ip addresses
+        ip_src = '.'.join(map(str, ip_header[8]))
+        ip_dst = '.'.join(map(str, ip_header[9]))
+        print("\nSource IP: {}\tDestination IP: {}".format(ip_src, ip_dst))
+        
+        if ip_protocol == 6:
+            # TCP packet
+            tcp_header = struct.unpack("!2s2s16s", packet[34:54])
+            tcp_data = packet[54:]
+
+            # Print the payload data
+            print("TCP header data: ", tcp_header)
+            print("TCP payload data:", tcp_data)
+        elif ip_protocol == 17:
+            # UDP packet
+            udp_header = struct.unpack("!4s4s", packet[34:42])
+            udp_data = packet[42:]
+
+            # Print the payload data
+            print("UDP header data: ", udp_header)
+            print("UDP payload data:", udp_data)
