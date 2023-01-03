40eb137b8bcb281a23b24f460efea75657705694
3nt0 <53119527+2nt0@users.noreply.github.com>
2023-01-03 00:36:05 +0000

    Update v2.py

--- v2.py
+++ v2.py
@@ -2,7 +2,10 @@ from scapy.all import sniff
 print("imported scapy")
 
 def packet_callback(packet):
-  print(packet.show())
+  src_mac = packet[Ether].src
+  dst_mac = packet[Ether].dst
+  print("Source MAC:", src_mac)
+  print("Destination MAC:", dst_mac)
 
 print("defined pack cb")
 
