8b841355463bc4b4e782698fdf40cfae9f65766a
3nt0 <53119527+2nt0@users.noreply.github.com>
2023-01-03 00:37:03 +0000

    Update v2.py

--- v2.py
+++ v2.py
@@ -1,12 +1,10 @@
 from scapy.all import sniff
-print("imported scapy")
+from scapy.layers.l2 import Ether
 
 def packet_callback(packet):
   src_mac = packet[Ether].src
   dst_mac = packet[Ether].dst
   print("Source MAC:", src_mac)
   print("Destination MAC:", dst_mac)
-
-print("defined pack cb")
 
 sniff(iface="wlan0", filter="ip", promisc=True, prn=packet_callback)
