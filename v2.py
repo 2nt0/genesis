b374cf8813d7c21f9da4fd72668b0f973ece242f
3nt0 <53119527+2nt0@users.noreply.github.com>
2023-01-03 00:34:39 +0000

    Update v2.py

--- v2.py
+++ v2.py
@@ -1,6 +1,9 @@
 from scapy.all import sniff
+print("imported scapy")
 
 def packet_callback(packet):
   print(packet.show())
 
-sniff(iface="eth0", filter="ip", promisc=True, prn=packet_callback)
+print("defined pack cb")
+
+sniff(iface="wlan0", filter="ip", promisc=True, prn=packet_callback)
